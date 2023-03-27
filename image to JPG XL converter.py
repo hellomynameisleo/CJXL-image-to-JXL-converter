f6e40be5f39808785382cb0015bf4f6e08eaa99f
Leo <31495285+hellomynameisleo@users.noreply.github.com>
2023-03-27 23:23:01 +0100

    Update image to JPG XL converter.py

--- image to JPG XL converter.py
+++ image to JPG XL converter.py
@@ -47,29 +47,41 @@ def process_image(file):
     output_file.parent.mkdir(parents=True, exist_ok=True)
     temporary_file.parent.mkdir(parents=True, exist_ok=True)
     
-    # Move the image to the temporary directory
     file.rename(output_file)
 
     async def compress_and_decode():
         try:
-            # Compress the image using cjxl
             subprocess.run(["cjxl", str(output_file), "--distance", "0", "--effort", "9", str(temporary_file)], stderr=subprocess.PIPE, check=True)
-
-            # Creates an object called output_file2 with the work temp added to output_file, decodes the processed image from cjxl to the temporary file output_file2 with the same extension as output_file
+        
+        except subprocess.CalledProcessError as error:
+            with log_file_error.open(mode='a', encoding='utf-8') as f:
+                current_time = datetime.now().strftime("Date=%d-%m-%Y | Time=%H:%M:%S")
+                error_msg = error.stderr.decode('utf-8').strip()
+                f.write(f"{current_time} | {input_file} - Failed: {error_msg}\n{'-' * 260}\n")
+                print(f"{input_file} - Failed: {error_msg}")
+                output_file.rename(destination_file)
+                print(f"Moving {output_file}")
+                temporary_file.unlink()
+        except Exception as e:
+            with log_file_error.open(mode='a', encoding='utf-8') as f:
+                current_time = datetime.now().strftime("Date=%d-%m-%Y | Time=%H:%M:%S")
+                f.write(f"{current_time} | {input_file} - Failed: {str(e)}\n{'-' * 260}\n")
+                print(f"{input_file} - Failed: {str(e)}")
+                output_file.rename(destination_file)
+                temporary_file.unlink()
+            raise
+        else:
             output_file2 = output_file.with_name(output_file.stem + temp + output_file.suffix)
             decoded_file = output_file2.with_suffix(file.suffix)
             subprocess.run(["djxl", str(temporary_file), str(decoded_file)], stderr=subprocess.PIPE, check=True)
-            # Run magick compare command and log the result
             compare_cmd = ['magick', 'compare', '-metric', 'ae', str(output_file), str(decoded_file), 'NUL']
             result = subprocess.run(compare_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			# If magick compare returns a 0 then move the cjxl processed temporary_file to the original file location
             if result.returncode == 0:
                 with log_file.open(mode='a', encoding='utf-8') as f:
                     current_time = datetime.now().strftime("Date=%d-%m-%Y | Time=%H:%M:%S")
                     f.write(f"{current_time} | {destination_file} - Done: magick compare returned {result.returncode}\n")
                     print(f"{destination_file} - Done")
                     temporary_file.rename(destination_file.with_suffix('.jxl'))
-					# Removes the original moved image file and output_file2 temp file
                     decoded_file.unlink()
                     output_file.unlink()
                 total_counter['processed'] += 1
@@ -79,40 +91,11 @@ def process_image(file):
                     current_time = datetime.now().strftime("Date=%d-%m-%Y | Time=%H:%M:%S")
                     f.write(f"{current_time} | {input_file} - Failed: magick compare returned {result.returncode}\n{'-' * 260}\n")
                     print(f"{input_file} - Failed: magick compare returned {result.returncode}")
-                    # Preserve the source file if magick compare returns an error
                     output_file.rename(destination_file)
-					# Removes the output_file2 temp file and cjxl processed temporary_file file
                     decoded_file.unlink()
                     temporary_file.unlink()
-
-        except subprocess.CalledProcessError as error:
-            # Log the error message and continue processing remaining files
-            error_msg = error.stderr.decode('utf-8').strip()
-            with log_file_error.open(mode='a', encoding='utf-8') as f:
-                current_time = datetime.now().strftime("Date=%d-%m-%Y | Time=%H:%M:%S")
-                f.write(f"{current_time} | {input_file} - Failed: {error_msg}\n{'-' * 260}\n")
-                print(f"{input_file} - Failed: {error_msg}")
-                # Preserve the source file if subprocess returns an error
-                output_file.rename(destination_file)
-        except Exception as e:
-            # Log any other exceptions and raise them
-            with log_file_error.open(mode='a', encoding='utf-8') as f:
-                current_time = datetime.now().strftime("Date=%d-%m-%Y | Time=%H:%M:%S")
-                f.write(f"{current_time} | {input_file} - Failed: {str(e)}\n{'-' * 260}\n")
-                print(f"{input_file} - Failed: {str(e)}")
-                # Preserve the source file if there is any other exception
-                output_file.rename(destination_file)
-            raise
-        finally:
-            # Move the image back to the original location and delete the temporary files
-            if output_file.exists() and result.returncode == 0:
-                output_file.rename(file.with_suffix('.jxl'))
-            elif destination_file.exists() and result.returncode != 0:
-                destination_file.rename(file)
-            if temporary_file.exists():
-                temporary_file.unlink
     asyncio.run(compress_and_decode())
-	
+         
 total_counter = Counter()
 start_time = time.time()
 with concurrent.futures.ThreadPoolExecutor(max_workers=23) as executor:
@@ -125,7 +108,6 @@ elapsed_time = time.time() - start_time
 hours, remainder = divmod(elapsed_time, 3600)
 minutes, seconds = divmod(remainder, 60)
 
-# Remove the temporary directory if it exists
 if temporary_path.exists():
     shutil.rmtree(temporary_path)
 
